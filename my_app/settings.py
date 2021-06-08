640ae1b0ff4c58c19f324aaa268302cfb65b352e
allan <allanfuenpi@criptext.com>
2021-06-08 14:07:08 -0500

    base updated.

--- my_app/settings.py
+++ my_app/settings.py
@@ -128,10 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
-
 STATICFILES_DIRS = [
-    BASE_DIR/'static',
+    BASE_DIR/'static/images',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
